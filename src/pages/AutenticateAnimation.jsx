9a8a80877b7e23177dcb891f6d6c47637741b104
Omar <omarnataren@outlook.com>
2025-05-08 15:28:13 -0600

    hotfix

--- src/pages/AutenticateAnimation.jsx
+++ src/pages/AutenticateAnimation.jsx
@@ -5,9 +5,8 @@ const AutenticateAnimation = ({ animationTriggered }) => {
 
 
   return (
-    <div className={`checkmark-container ${animationTriggered ? 'rotate' : ''}`}>
-            
-                <div className={`overlay ${animationTriggered ? 'active' : ''}`}>
+    <div className={`checkmark-container ${animationTriggered ? 'rotate' : ''}`}>    
+        <div className={`overlay ${animationTriggered ? 'active' : ''}`}>
               <div className="circle">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -21,11 +20,11 @@ const AutenticateAnimation = ({ animationTriggered }) => {
                   strokeLinejoin="round"
                   className={`checkmark-icon ${animationTriggered ? 'rotate' : ''}`}
                 >
-                  <path d="M20 6L9 17l-5-5" />
+                    <path d="M20 6L9 17l-5-5" />
                 </svg>
-              </div>
             </div>
-            </div>
+        </div>
+    </div>
   );
 };
 
